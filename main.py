7a076951450d22bc6236dd0bdbd6bdc04ecbbd1c
raymondmalcolm <ray@malcolm.net>
2026-04-17 17:12:54 -0500

    Update main.py
    Initial code. More to come

--- main.py
+++ main.py
@@ -5,34 +5,58 @@
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
 
-
-from datetime import datetime
-import pandas
+from calendar import FRIDAY, MONDAY, SATURDAY, SUNDAY, THURSDAY, TUESDAY, WEDNESDAY
+import datetime as dt
 import random
 import smtplib
+import pandas as pd
+from pathlib import Path
 import os
 
 # import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+password = PASSWORD
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+now = dt.datetime.now()
+day_of_week = now.weekday()
+#print(day_of_week)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+# 2. Check if today matches a birthday in the birthdays.csv
+
+directory_path = Path('letter_templates')
+letters = []
+
+try:
+    df  = pd.read_csv("birthdays.csv")
+    birthday_dict = df.to_dict(orient='records')
+except FileNotFoundError:
+    print("File not found.")
+
+# iterate over directory items and filter for files
+for file_path in directory_path.iterdir():
+    if file_path.is_file():
+        with file_path.open('r', encoding='utf-8') as f:
+            letters.append(f.read())
+
+for birthday in birthday_dict:
+    if birthday["Month"] == now.month and birthday["Day"] == now.day:
+        letter = random.choice(letters)
+        letter = letter.replace("[NAME]", birthday["Name"])
+
+        # 4. Send the letter generated in step 3 to that person's email address.
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email, 
+                                to_addrs="ray@malcolm.net", 
+                                msg=f"Subject:Happy Birthday\n\n{letter}"
+
         )
+
+
+
+
+

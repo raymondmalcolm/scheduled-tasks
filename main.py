34b7bfc80280f0a757fe570c89ab88e9a1aabd93
raymondmalcolm <ray@malcolm.net>
2026-04-17 19:39:52 -0500

    Remove unused calendar imports from main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
 
-from calendar import FRIDAY, MONDAY, SATURDAY, SUNDAY, THURSDAY, TUESDAY, WEDNESDAY
 import datetime as dt
 import random
 import smtplib
